426e1d08aeea82e9cdea606fec45ee8811f1569f
bekruz-pardayev <behruzp330@gmail.com>
2024-06-24 09:43:46 +0500

    lesson2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import { Component } from "react";
+import { Form } from "./Form";
 
 export class App extends Component {
   state = {
     count: 0,
+    users:[]
   };
+  componentDidMount (){
+    //yomponet yaralganda faqat 1 marta ishlaydi
+    fetch("https://jsonplaceholder.typicode.com/users").then(data=>data.json()).then(data=>this.setState({users:this.users=data}))
+  }
+  componentDidUpdate(){
+    //companenet yangilangdanda ishlaydi
+    console.log("did update");
+  }
+  componentWillUnmount(){
+    //component ulish jarayonida ishlaydi
+    console.log("will unmount");
+  }
   render() {
-    console.log(this.state.count);
-
+    // console.log(this.state.count);
+    console.log(this.state.users);
     return (
       <>
         <h1>{this.state.count}</h1>
@@ -17,7 +31,12 @@ export class App extends Component {
         >
           click
         </button>
+        <Form/>
       </>
     );
   }
 }
+
+
+
+
